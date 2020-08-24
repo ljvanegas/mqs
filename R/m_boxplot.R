bd1ef0c4a125629b687e06299e3854eefd9253fd
Laura Jula Vanegas <53752344+ljvanegas@users.noreply.github.com>
2020-08-24 21:54:15 +0200

    Update m_boxplot.R
    Update noncrossing quantiles

--- R/m_boxplot.R
+++ R/m_boxplot.R
@@ -1,10 +1,17 @@
-mqs.boxplot <- function(y, q = NULL, alpha = 0.05, type = "runs"){
+mqs.boxplot <- function(y, q = NULL, alpha = 0.05, type = "runs", intersect = FALSE){
   
-  sol.low <- mqse(y, beta = 0.25, q[1], alpha, type = type)
-  sol.med <- mqse(y, beta = 0.5, q[2], alpha, type = type)
-  sol.high <- mqse(y, beta = 0.75, q[3], alpha, type = type)
+  sol.low <- mqse(y, beta = 0.25, q[1], alpha, type = type, conf = TRUE)
+  sol.med <- mqse(y, beta = 0.5, q[2], alpha, type = type, conf = TRUE)
+  sol.high <- mqse(y, beta = 0.75, q[3], alpha, type = type, conf = TRUE)
   
-  ans = list(lowerQ = fitted(sol.low), median = fitted(sol.med), higherQ = fitted(sol.high))
+  if(intersect == FALSE){
+    aux <- .Iint(sol.low, sol.med, sol.high, y)
+  }
+  else{
+    aux <- list(sol.low, sol.med, sol.high)
+  }
+  
+  ans = list(lowerQ = fitted(aux[[1]]), median = fitted(aux[[2]]), higherQ = fitted(aux[[3]]))
   attr(ans, "class") =  "mqsBox"
   return(ans)
 }
